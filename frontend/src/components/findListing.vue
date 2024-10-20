3b070ff9c74369c87070cb6b6268ffb5722f6c2c
Kai-Tay <kaishengtay82@gmail.com>
2024-10-20 17:08:39 +0800

    Updated findListing path.

--- frontend/src/components/findListing.vue
+++
@@ -1,5 +0,0 @@
-<script setup>
-import Navbar from './Navbar.vue';
-import { db } from '../firebase.js';
-import {collection, doc, getDoc, getDocs} from "firebase/firestore";
-</script>
--- frontend/src/main.js
+++ frontend/src/main.js
@@ -5,7 +5,7 @@ import {createRouter, createWebHashHistory} from 'vue-router'
 import Home from './components/Home.vue';
 import Chat from './components/chat.vue';
 import BuyerDashboard from './components/buyerDashboard.vue';
-import FindListings from './components/findListing.vue';
+import FindListings from './components/Restaurant/FindListings.vue';
 import './firebase.js';
 
 
