8639149cf9a51634bcfdc9123025ca19d3a5f842
Kai-Tay <kaishengtay82@gmail.com>
2024-10-29 20:52:20 +0800

    Added Checkout (Half Done)

--- frontend/src/components/Restaurant/viewSupplier.vue
+++ frontend/src/components/Restaurant/viewSupplier.vue
@@ -11,31 +11,14 @@ import {
 import { doc, getDoc } from "firebase/firestore";
 import { db } from '../../firebase.js';
 import { useRoute } from 'vue-router';
+import { Stepper, StepperDescription, StepperIndicator, StepperItem, StepperSeparator, StepperTitle, StepperTrigger, } from '@/components/ui/stepper'
 </script>
 
 <template>
-    <header class="bg-gray-900 py-5 h-80 justify-center">
-        <div class="px-5 row gx-5 justify-center">
-            <div class="col-lg-6">
-                <div class="text-center my-10 ">
-                    <!-- Welcome Text -->
-                    <div class="text-4xl font-bold">
-                        <h1 class="display-5 fw-bolder text-white mb-2">Find Suppliers</h1>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </header>
-
-
     <!-- Filter Bar from Search -->
 
-    <div class="mt-5 mb-5 mx-5">
+    <div class="mt-5 mb-5 mx-5 space-y-10">
         <div class="text-4xl font-bold h-10">{{ supplierListing.supplierName }}</div>
-    </div>
-
-    <!-- Listings -->
-    <div class="grid grid-cols-1 gap-4 mx-5">
         <Card>
             <CardHeader>
                 <CardTitle>
@@ -44,60 +27,98 @@ import { useRoute } from 'vue-router';
                 <CardDescription class="text-lg mt-4">{{ supplierListing.supplierDescription }}</CardDescription>
             </CardHeader>
         </Card>
-
-        <Card>
-            <CardHeader>
-                <CardTitle>Inventory</CardTitle>
-                <CardDescription>
-                    Select your items
-                </CardDescription>
-            </CardHeader>
-            <CardContent>
-                <Table>
-                    <TableHeader>
-                        <TableRow>
-                            <TableHead >
-                                Name
-                            </TableHead>
-                            <TableHead class="w-[100px]">Stock</TableHead>
-                            <TableHead class="w-[200px]">Price Per Unit</TableHead>
-                            <TableHead class="">
-                                Quantity
-                            </TableHead>
-                        </TableRow>
-                    </TableHeader>
-                    <!-- For Loop Here -->
-                    <TableBody v-for="item in supplierListing.inventory">
-                        <TableRow>
-                            <TableCell class="font-semibold">
-                                {{ item.productName }}
-                            </TableCell>
-                            <TableCell>
-                                <Label for="stock-1" class="sr-only">
-                                    Stock
-                                </Label>
-                                {{ item.quantity }}
-                            </TableCell>
-                            <TableCell>
-                                <Label for="price-1" class="sr-only">
-                                    Price
-                                </Label>
-                                ${{  item.pricePerUnit }}   
-                            </TableCell>
-                            <TableCell>
-                                <Label for="price-1" class="sr-only">
-                                    Qty
-                                </Label>
-                                <Input id="price-1" type="number" default-value="0" min="0" :max="item.quantity"/>
-                            </TableCell>
-                        </TableRow>
-                    </TableBody>
-                </Table>
-            </CardContent>
-        </Card>
     </div>
 
 
+    <div class="sm:flex justify-center my-10 hidden ">
+        <Stepper v-model="activeStep">
+            <StepperItem v-for="item in steps" :key="item.step" class="" :step="item.step">
+                <StepperTrigger @click="goToStep(item.step)">
+                    <StepperIndicator>
+                        <component :is="item.icon" class="w-4 h-4" />
+                    </StepperIndicator>
+                    <div class="flex flex-col">
+                        <StepperTitle>
+                            {{ item.title }}
+                        </StepperTitle>
+                        <StepperDescription>
+                            {{ item.description }}
+                        </StepperDescription>
+                    </div>
+                </StepperTrigger>
+            </StepperItem>
+        </Stepper>
+    </div>
+
+    <!-- Step Content Based on Active Step -->
+    <div v-if="activeStep === 1">
+        <!-- Listings -->
+        <div class="grid grid-cols-1 gap-4 mx-5">
+            <Card>
+                <CardHeader>
+                    <CardTitle>Inventory</CardTitle>
+                    <CardDescription>
+                        Select your items
+                    </CardDescription>
+                </CardHeader>
+                <CardContent>
+                    <Table>
+                        <TableHeader>
+                            <TableRow>
+                                <TableHead>
+                                    Name
+                                </TableHead>
+                                <TableHead class="w-[100px]">Stock</TableHead>
+                                <TableHead class="w-[200px]">Price Per Unit</TableHead>
+                                <TableHead class="">
+                                    Quantity
+                                </TableHead>
+                            </TableRow>
+                        </TableHeader>
+                        <!-- For Loop Here -->
+                        <TableBody v-for="(item, index) in supplierListing.inventory" :key="item.id">
+                            <TableRow>
+                                <TableCell class="font-semibold">
+                                    {{ item.productName }}
+                                </TableCell>
+                                <TableCell>
+                                    <Label :for="`stock-${index}`" class="sr-only">
+                                        Stock
+                                    </Label>
+                                    {{ item.quantity }}
+                                </TableCell>
+                                <TableCell>
+                                    <Label :for="`price-${index}`" class="sr-only">
+                                        Price
+                                    </Label>
+                                    ${{ item.pricePerUnit }}
+                                </TableCell>
+                                <TableCell>
+                                    <Label :for="`qty-${index}`" class="sr-only">
+                                        Qty
+                                    </Label>
+                                    <Input :id="`qty-${index}`" type="number" default-value="" min="1" :max="item.quantity" />
+                                </TableCell>
+                            </TableRow>
+                        </TableBody>
+                    </Table>
+                </CardContent>
+            </Card>
+        </div>
+    </div>
+    <div v-else-if="activeStep === 2">
+        <p>Content for Step 2</p>
+    </div>
+    <div v-else-if="activeStep === 3">
+        <p>Content for Step 3</p>
+    </div>
+
+
+
+    <div class="flex flex-inline justify-center my-4 space-x-20">
+        <Button @click="previousStep" variant="grey">Previous</Button>
+        <Button @click="nextStep" :disabled="activeStep === steps.length">Continue</Button>
+    </div>
 
 </template>
 
@@ -110,18 +131,66 @@ export default {
 
     data() {
         return {
+            //Stepper
+            activeStep: 1,
+            steps: [{
+                step: 1,
+                title: 'Add to Cart',
+                description: 'Select your items!',
+                icon: null,
+            }, {
+                step: 2,
+                title: 'Checkout',
+                description: 'Confirm your Order',
+                icon: null,
+            }, {
+                step: 3,
+                title: 'Delivery Information',
+                description: 'Provide delivery information',
+                icon: null,
+            }, {
+                step: 4,
+                title: 'Payment',
+                description: 'Pay for your order',
+                icon: null,
+            }],
             supplierId: "",
             supplierListing: {},
+
+            orderCart: {},
         };
     },
     mounted() {
+        // Get the supplier ID from the URL
         const route = useRoute();
         this.supplierId = route.params.id;
+
         // Obtain Database Suppliers
         this.fetchListing();
     },
 
     methods: {
+        // Stepper Methods
+        goToStep(index) {
+            this.activeStep = index;
+        },
+        nextStep() {
+            if (this.activeStep == 1){
+                this.handleInventoryQuantity();
+            }
+            if (this.activeStep < this.steps.length) {
+                this.activeStep++;
+            }
+        },
+        previousStep() {
+            if (this.activeStep > 1) {
+                this.activeStep--;
+            } else {
+                this.$router.push("/find");
+            }
+        },
+
+        // Database Methods
         async fetchListing() {
             // Fetch listings from the database supplierListing and users
             try {
@@ -133,10 +202,10 @@ export default {
                 const userRef = doc(db, "users", this.supplierId);
                 const userSnap = await getDoc(userRef);
 
-                // Combine the data
+                // Combine the data (Add company description from user database to merged data)
                 const mergedData = {
-                    ...listingSnap.data(), // Spread listing data
-                    supplierDescription: userSnap.data().companyDescription  // Spread company data
+                    ...listingSnap.data(),
+                    supplierDescription: userSnap.data().companyDescription
                 };
 
                 this.supplierListing = mergedData;
@@ -145,6 +214,14 @@ export default {
                 console.error('Error fetching listings:', error)
             }
         },
+
+
+        // On Qty Change
+        handleInventoryQuantity() {
+            // Get the qty values for each listing and update the orderCart
+            const qtyInputs = document.querySelectorAll('input[type="number"]');
+            console.log(qtyInputs);
+        }
     },
 };
 </script>
--- frontend/src/components/ui/button/index.js
+++ frontend/src/components/ui/button/index.js
@@ -9,6 +9,7 @@ export const buttonVariants = cva(
       variant: {
         default: 'bg-primary text-primary-foreground hover:bg-primary/90',
         green: 'bg-green-600 text-primary-foreground hover:bg-green-600/90',
+        grey: 'bg-gray-600 text-primary-foreground hover:bg-gray-600/90',
         destructive:
           'bg-destructive text-destructive-foreground hover:bg-destructive/90',
         outline:
