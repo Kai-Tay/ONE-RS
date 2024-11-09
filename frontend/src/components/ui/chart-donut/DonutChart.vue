523be3ee8700eee975c447fa235f1c67dc18d1df
GeraldYeong04 <geraldyeong.2023@scis.smu.edu.sg>
2024-11-09 21:04:22 +0800

    Updated OrderHistoryDashboard (WIP)

--- frontend/src/components/ui/chart-donut/DonutChart.vue
+++ frontend/src/components/ui/chart-donut/DonutChart.vue
@@ -14,7 +14,7 @@ const props = defineProps({
     showLegend: { type: Boolean, required: false, default: true },
     showTooltip: { type: Boolean, required: false, default: true },
     filterOpacity: { type: Number, required: false, default: 0.2 },
-    category: { type: String, required: true },
+    category: { type: Array, required: true },
     type: { type: String, required: false, default: 'donut' },
     sortFunction: { type: Function, required: false, default: () => undefined },
     valueFormatter: { type: Function, required: false, default: (tick) => `${tick}` },
