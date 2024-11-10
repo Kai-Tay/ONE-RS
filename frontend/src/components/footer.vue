7bad531c530bec051808f20b590e5927d7905568
Kai-Tay <kaishengtay82@gmail.com>
2024-11-10 22:51:01 +0800

    Fixed footer.vue

--- frontend/src/components/footer.vue
+++ frontend/src/components/footer.vue
@@ -1,11 +1,11 @@
 <template>
-    <footer class="bg-gray-900 text-white py-8">
-        <div class="container mx-auto text-center space-y-4 ">
+    <footer class="bg-gray-900 text-white py-8 px-16 lg:px-28">
+        <div class="mx-auto text-center space-y-4 ">
             <!-- Website Name -->
             <h2 class="text-2xl font-bold text-white">ONE.RS 🧑‍🍳</h2>
 
             <!-- Contact Information -->
-            <div class="md:px-28 text-sm md:text-lg">
+            <div class=" text-sm md:text-lg">
                 <p class="text-gray-400 ">Effortless sourcing for restaurant owners. </p>
                 <p class="text-gray-400 ">Discover and connect with reliable suppliers, simplify orders, and ensure
                     your kitchen is always stocked with the finest ingredients.</p>
